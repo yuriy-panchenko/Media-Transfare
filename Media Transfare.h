d9d5501a3d506f553cee8a972a8b1a5c3f166c62
yuran <yuran@DESKTOP-CRKF3S0>
2024-11-02 04:20:22 +0300

    Add project files.

---
+++ Media Transfare.h
@@ -0,0 +1,32 @@
+
+// Media Transfare.h : main header file for the PROJECT_NAME application
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'pch.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// main symbols
+
+
+// CMediaTransfareApp:
+// See Media Transfare.cpp for the implementation of this class
+//
+
+class CMediaTransfareApp : public CWinApp
+{
+public:
+	CMediaTransfareApp();
+
+// Overrides
+public:
+	virtual BOOL InitInstance();
+
+// Implementation
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CMediaTransfareApp theApp;
