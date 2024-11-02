993eb059004a474c48ceb368283ee16259617e60
fockuss <yuranp@hotmail.com>
2024-11-02 05:03:50 +0300

    file types dlg

--- Resource.h
+++ Resource.h
@@ -7,6 +7,7 @@
 #define IDS_ABOUTBOX                    101
 #define IDD_MEDIA_TRANSFARE_DIALOG      102
 #define IDR_MAINFRAME                   128
+#define IDD_FILE_TYPES_DLG              131
 #define IDC_BROWSE_SRC                  1000
 #define IDC_INFO_SRC_1                  1001
 #define IDC_INFO_SRC_2                  1002
@@ -18,16 +19,20 @@
 #define IDC_FILE_TYPES                  1008
 #define IDC_AUTO_RENAME                 1009
 #define IDC_IGNORE_DUPLICATES           1010
-#define IDC_CHECK3                      1011
 #define IDC_SEARCH_SUB_FOLDERS          1011
+#define IDC_ADD                         1012
+#define IDC_EXT_ADD                     1012
+#define IDC_EXT_REMOVE                  1013
+#define IDC_EXT_EDIT                    1014
+#define IDC_EXT_LIST                    1015
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        131
+#define _APS_NEXT_RESOURCE_VALUE        133
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1012
+#define _APS_NEXT_CONTROL_VALUE         1016
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
